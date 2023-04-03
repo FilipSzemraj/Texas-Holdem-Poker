4bf8ef6563519cd87590e50bc121da0ff60f7aed
FilipSzemraj <filip.szemraj@onet.pl>
2023-04-04 00:25:46 +0200

    first version

--- src/main/java/org/main/LoginController.java
+++ src/main/java/org/main/LoginController.java
@@ -1,10 +1,9 @@
 package org.main;
 
-import Sql.DatabaseConnection;
+import sql.DatabaseConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -18,7 +17,6 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ResourceBundle;
 
 
 public class LoginController {
