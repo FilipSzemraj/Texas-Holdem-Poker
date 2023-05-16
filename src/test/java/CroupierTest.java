3e11bb4de7228150909e991d836cc48a5949dd5b
FilipSzemraj <filip.szemraj@onet.pl>
2023-05-16 19:31:37 +0200

    Wersja konsolowa oraz testy dla klasy CheckHand i Croupier.

--- src/test/java/CroupierTest.java
+++ src/test/java/CroupierTest.java
@@ -5,14 +5,14 @@ import static org.junit.Assert.*;
 public class CroupierTest {
     Croupier krupier = new Croupier(5);
     @Test
-    public void checkIfPlayerWithAllInHaveAction_oneWinner_true()
+    public void checkForAllHands_oneWinnerWithMaxBet_true()
     {
         //given
         krupier.dealCardsWithAssumptions_checkWhenWinningCardsOnTableAndMattersTheHighestCard();
 
         //when
         krupier.setIsInCurrentRoundForAllPlayers();
-        krupier.addMoneyToThePot_ForOneWinner();
+        krupier.addMoneyToThePot_ForEverybodyWithMaxBet();
         krupier.checkCurrentPlayingPlayers_makePublic();
         krupier.checkForAllHands_makePublic();
         int winnersAmountOfMoney=krupier.extractTheWinner_makePublic();
@@ -21,19 +21,69 @@ public class CroupierTest {
         assertEquals(900, winnersAmountOfMoney);
     }
     @Test
-    public void checkIfPlayerWithAllInHaveAction_ThreeWinners_true()
+    public void checkForAllHands_oneWinnerWithoutMaxBet_true()
     {
         //given
         krupier.dealCardsWithAssumptions_checkWhenWinningCardsOnTableAndMattersTheHighestCard();
 
         //when
         krupier.setIsInCurrentRoundForAllPlayers();
-        krupier.addMoneyToThePot_ForTwoWinners();
+        krupier.addMoneyToThePot_ForLastPlayerWithoutMaxBet();
         krupier.checkCurrentPlayingPlayers_makePublic();
         krupier.checkForAllHands_makePublic();
         int winnersAmountOfMoney=krupier.extractTheWinner_makePublic();
 
         //then
-        assertEquals(700, winnersAmountOfMoney);
+        assertEquals(250, winnersAmountOfMoney);
+    }
+
+    @Test
+    public void checkForAllHands_DrawTwoWinnersWithMaxBet_true()
+    {
+        //given
+        krupier.dealCardsWithAssumptions_checkWhenTwoPlayersHaveDraw();
+
+        //when
+        krupier.setIsInCurrentRoundForAllPlayers();
+        krupier.addMoneyToThePot_ForEverybodyWithMaxBet();
+        krupier.checkCurrentPlayingPlayers_makePublic();
+        krupier.checkForAllHands_makePublic();
+        int winnersAmountOfMoney=krupier.extractTheWinner_makePublic();
+
+        //then
+        assertEquals(650, winnersAmountOfMoney);
+    }
+    @Test
+    public void checkForAllHands_DrawTwoWinnersOneWithoutMaxBet_true()
+    {
+        //given
+        krupier.dealCardsWithAssumptions_checkWhenTwoPlayersHaveDraw();
+
+        //when
+        krupier.setIsInCurrentRoundForAllPlayers();
+        krupier.addMoneyToThePot_ForLastPlayerWithoutMaxBet();
+        krupier.checkCurrentPlayingPlayers_makePublic();
+        krupier.checkForAllHands_makePublic();
+        int winnersAmountOfMoney=krupier.extractTheWinner_makePublic();
+
+        //then
+        assertEquals(150, winnersAmountOfMoney);
+    }
+    @Test
+    public void checkForAllHands_DrawTwoWinnersOneWithoutMaxBetWithOnePlayerAllIn_true()
+    {
+        //given
+        krupier.dealCardsWithAssumptions_checkWhenTwoPlayersHaveDraw();
+
+        //when
+        krupier.setIsInCurrentRoundForAllPlayers();
+        krupier.addMoneyToThePot_ForLastPlayerWithoutMaxBet();
+        krupier.addMoneyToThePot_changeFirstPlayerToAllIn();
+        krupier.checkCurrentPlayingPlayers_makePublic();
+        krupier.checkForAllHands_makePublic();
+        int winnersAmountOfMoney=krupier.extractTheWinner_makePublic();
+
+        //then
+        assertEquals(150, winnersAmountOfMoney);
     }
 }
