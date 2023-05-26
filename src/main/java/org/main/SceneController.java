3c2fb78fcd78c2c0f320282b259276f09ca6c90b
FilipSzemraj <filip.szemraj@onet.pl>
2023-05-26 21:43:54 +0200

    Interfejs graficzny

--- src/main/java/org/main/SceneController.java
+++ src/main/java/org/main/SceneController.java
@@ -1,27 +1,9 @@
 package org.main;
 
-import Game.Croupier;
-import javafx.animation.FadeTransition;
-import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class SceneController{
     public Label PlayerName_label;
